642b0c4331b83def4394cadde203caa0e78e5456
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-05-03 17:27:52 +0100

    feat: Navigation links - active

--- components/Navigation/libs/navLinks.tsx
+++ components/Navigation/libs/navLinks.tsx
@@ -1,4 +1,6 @@
 import React, { FC, HTMLAttributes } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { HomeIcon, SearchIcon, TemperatureIcon, MeatIcon } from "../../icons";
 
 type Props = HTMLAttributes<HTMLUListElement>;
@@ -12,40 +14,40 @@ const navLinks = [
   {
     name: "Discover recipes",
     icon: <SearchIcon />,
+    url: "/discover",
   },
   {
     name: "Temperature guide",
     icon: <TemperatureIcon />,
+    url: "/temperatures",
   },
   {
     name: "Protein guide",
     icon: <MeatIcon />,
+    url: "/protein",
   },
 ];
 
-/**
- * TODO
- * 1. Change color to default gray - DONE
- * 2. Add Hover effects - DONE
- * 3. Change active link to match current page <- Later!
- */
-
 export const NavLinks: FC<Props> = () => {
+  const router = useRouter();
+
   return (
     <ul className="ml-6 pt-4">
       {navLinks.map((link) => (
         <li
           key={link.name}
           className={`font-medium text-gray-700 w-full py-1 cursor-pointer hover:text-green-600 ${
-            link.url === "/"
+            link.url === router.pathname
               ? "text-green-600 border-r-4 border-r-green-700"
               : ""
           } hover:scale-105 transition-all`}
         >
-          <a className="flex items-center">
-            {link.icon}
-            <span className="ml-2">{link.name}</span>
-          </a>
+          <Link href={link.url}>
+            <a className="flex items-center">
+              {link.icon}
+              <span className="ml-2">{link.name}</span>
+            </a>
+          </Link>
         </li>
       ))}
     </ul>
